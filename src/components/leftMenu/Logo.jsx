d1eee3c1edd06cb4592a270ff3c40b20e3a9131d
Daniel Dąbrowski <elosik123@onet.pl>
2019-05-04 11:55:47 +0200

    Finished LeftMenu component

--- src/components/leftMenu/Logo.jsx
+++ src/components/leftMenu/Logo.jsx
@@ -5,11 +5,30 @@ const LogoContainer = styled.div`
     width: 100%;
     height: 130px;
     border-bottom: 2px solid #17181D;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const LogoIcon = styled.div`
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    background: #343944;
+    color: #ffffff;
+    font-family: 'PT Serif', serif;
+    font-size: 26px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: default;
 `
 
 const Logo = () => {
     return (
-        <LogoContainer></LogoContainer>
+        <LogoContainer>
+            <LogoIcon>N</LogoIcon>
+        </LogoContainer>
     );
 };
 
