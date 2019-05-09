caeb7b2c6ffd1aa2b1c7a5664046b84e93b7b5d9
Daniel Dąbrowski <elosik123@onet.pl>
2019-05-09 17:44:59 +0200

    Updates

---
+++ precache-manifest.618119d265f13e767b62a734f93080c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7ff14e836501fc80acb7f7421f5653",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "76e2086163824e2da113",
+    "url": "/notes/static/css/main.dc649c6d.chunk.css"
+  },
+  {
+    "revision": "1b3456d1f49302455028",
+    "url": "/notes/static/js/2.6fd873e8.chunk.js"
+  },
+  {
+    "revision": "76e2086163824e2da113",
+    "url": "/notes/static/js/main.c6bac943.chunk.js"
+  },
+  {
+    "revision": "01155016b6fc9fe91e5f",
+    "url": "/notes/static/js/runtime~main.21ba3d73.js"
+  }
+]);
